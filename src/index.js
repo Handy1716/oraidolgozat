87eef1fae2acdc857f83c25c9f59a949a04a7e99
Polgár Kristóf <polgarkristof10@gmail.com>
2022-09-28 13:10:31 +0200

    rewdsads

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+function kepUrl() {
+    var element = document.img;
+    element.classList.toggle("dark-mode");
+ }
+
+
+
+ 
 document.getElementById("darkMode").addEventListener("click", darkMode);
+document.getElementById("darkMode").addEventListener("click", darkMode);
+document.getElementById("kepurl").addEventListener("change", darkMode);
 
 
 
