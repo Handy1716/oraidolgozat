bae273204a5a8751381dfdb8cf417e890cfec06a
Polgár Kristóf <polgarkristof10@gmail.com>
2022-09-28 14:40:30 +0200

    dasdsa

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function darkMode() {
  }
 function kepUrl() {
    // var element = document.img;
-   // document.getElementById("kep").src = "hackanm.gif";
+    document.getElementById("kep").src = document.getElementById("kepurl").value;
     document.getElementById("kep").style.width = document.getElementById("szelesseg").value + 'px';
     document.getElementById("kep").style.border = document.getElementById("keret").value + 'px solid';
     document.getElementById("kep").style.borderColor = document.getElementById("szin").value;
@@ -19,6 +19,7 @@ function kepUrl() {
  
 document.getElementById("darkMode").addEventListener("click", darkMode);
 document.getElementById("darkMode").addEventListener("click", darkMode);
+document.getElementById("kepurl").addEventListener("keyup", kepUrl);
 document.getElementById("szelesseg").addEventListener("keyup", kepUrl);
 document.getElementById("keret").addEventListener("keyup", kepUrl);
 document.getElementById("szin").addEventListener("keyup", kepUrl);
